f141611882957ac4a80f62f43e069035e9a0b0fb
zizou161 <aziz.rcb2011@gmail.com>
2019-11-21 00:44:09 +0100

    mysql ready

---
+++ IGL_project/database/migrations/2019_11_20_200920_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table ->String('Nom');
+            $table ->String('Prénom');
+            $table ->String('Adresse-email');
+            $table ->date('Date de naissance');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
